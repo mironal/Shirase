de64f77652d0c3b8a98caeb907d9c8e9c94f0f75
mironal <miro.gunp@gmail.com>
2019-04-09 23:19:51 +0900

    Notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from "react"
+import React, { useEffect, useReducer, useCallback, useRef } from "react"
 import "./App.css"
 import Sidebar from "./Sidebar"
 import Viewer from "./Viewer"
@@ -6,16 +6,33 @@ import { reducer, initialState } from "./store"
 import storage from "./storage"
 import Octokit from "@octokit/rest"
 
+const lastModified: { [url: string]: string } = {}
+
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState())
 
-  const fetchNotifications = async () => {
+  const fetchNotifications = useCallback(async () => {
     const { url, personalToken } = state.ghConfigs[state.currentUrl]
     if (personalToken.length > 0) {
       const client = new Octokit({
         baseUrl: url,
         auth: personalToken,
       })
+
+      let firstRequest = true
+      if (lastModified[url]) {
+        firstRequest = false
+        client.hook.before("request", option => {
+          option.headers["If-Modified-Since"] = lastModified[url]
+        })
+
+        client.hook.error("request", error => {
+          if (error.status === 304) {
+            return state.notifications
+          }
+          throw error
+        })
+      }
 
       const result = await client.activity
         .listNotifications({ all: true })
@@ -26,12 +43,18 @@ function App() {
       if (result) {
         dispatch({ type: "clearError", url })
         dispatch({ type: "setLastUpdate", url, date: new Date() })
+        // TODO 差分を表示 or 新しいものを強調表示
         if (result.status === 200) {
+          lastModified[url] = result.headers["last-modified"]
           dispatch({ type: "setNotification", url, notifications: result.data })
+
+          if (!firstRequest && Notification.permission === "granted") {
+            new Notification(`New notification ${result.data[0].subject.title}`)
+          }
         }
       }
     }
-  }
+  }, [])
 
   useEffect(() => {
     if (Object.keys(state.ghConfigs).length === 0) {
@@ -42,6 +65,13 @@ function App() {
   }, [state.ghConfigs])
 
   useEffect(() => {
+    if (Notification.permission === "default") {
+      Notification.requestPermission(permission => {
+        if (permission === "granted") {
+          new Notification("Thank you ₍₍ (ง ˙ω˙)ว ⁾⁾")
+        }
+      })
+    }
     const id = setInterval(() => {
       fetchNotifications()
     }, 1000 * 60)
